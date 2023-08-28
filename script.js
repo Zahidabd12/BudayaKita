b136b870dda4fc1b84468e763dc3815d9c7184ba
Ashtacon <astacialvndr@gmail.com>
2023-08-28 23:59:59 +0700

    penyeseuaian ulang animasi hero-image

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ gsap.registerPlugin(TextPlugin);
 gsap.to(".introduction", {duration: 3, y:1500, opacity: 1});
 gsap.to(".contain", {duration: 2, delay: .5, text: 'Pesona<br>Warisan'});
 gsap.from(".ani",{ duration: 3, y:55, opacity: 0.5});
-gsap.from(".megamendung", {duration: 2, y:-150, opacity: 0});
+gsap.from(".megamendung", {duration: 2, y:-150, opacity: 0.1});
+gsap.from(".navbar", {duration: 2.5, y:60, opacity: 1});
 
 
 $(document).on("scroll",function(){
