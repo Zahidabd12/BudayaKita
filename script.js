1f69785bcb9748fe3ed50e65f0bc43e8b42debe1
Ashtacon <astacialvndr@gmail.com>
2023-08-28 23:27:46 +0700

    menyesuaikan animasi

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 gsap.registerPlugin(TextPlugin);
 gsap.to(".introduction", {duration: 3, y:1000, opacity: 1});
 gsap.to(".contain", {duration: 2, delay: .5, text: 'BudayaKita'});
-gsap.from(".ani",{ duration: 1.5, y:25, opacity: 0.5});
+gsap.from(".ani",{ duration: 3, y:55, opacity: 0.5});
 gsap.from(".megamendung", {duration: 2, y:-150, opacity: 0});
 
 
