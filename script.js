6751cc3f46e27c432bf3659290fb4a41876f4dab
Ashtacon <astacialvndr@gmail.com>
2023-08-28 23:40:16 +0700

    memperbaiki animasi dan css

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 gsap.registerPlugin(TextPlugin);
-gsap.to(".introduction", {duration: 3, y:1000, opacity: 1});
-gsap.to(".contain", {duration: 2, delay: .5, text: 'BudayaKita'});
+gsap.to(".introduction", {duration: 3, y:1500, opacity: 1});
+gsap.to(".contain", {duration: 2, delay: .5, text: 'Pesona<br>Warisan'});
 gsap.from(".ani",{ duration: 3, y:55, opacity: 0.5});
 gsap.from(".megamendung", {duration: 2, y:-150, opacity: 0});
 
