0e353d057c8355a643e922a3cb5a81c90e666803
Ashtacon <astacialvndr@gmail.com>
2023-08-28 23:24:49 +0700

    menambahkan sedikit animasai pada hero-image

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-$(document).ready(function(){
+gsap.registerPlugin(TextPlugin);
+gsap.to(".introduction", {duration: 3, y:1000, opacity: 1});
+gsap.to(".contain", {duration: 2, delay: .5, text: 'BudayaKita'});
+gsap.from(".ani",{ duration: 1.5, y:25, opacity: 0.5});
+gsap.from(".megamendung", {duration: 2, y:-150, opacity: 0});
 
-//    Jquery Code here
 
-
-
+$(document).on("scroll",function(){
+  if ($(document).scrollTop() > 200){
+    $(".introduction").addClass("shrink");
+  }else {
+    $(".introduction").removeClass("shrink");
+  }
 });
