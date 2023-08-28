2d3525845a8c00a7e53101a70b5e4498d4154759
Ashtacon <astacialvndr@gmail.com>
2023-08-29 01:12:32 +0700

    menambahkan coment

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
+// Native Code
 window.onload = function() {
   window.scrollTo(0, 0);
 }
 
-
+// GSAP Animation
 gsap.registerPlugin(TextPlugin);
 gsap.to(".introduction", {duration: 3, delay:1.5, y:1500, opacity: 1});
 gsap.to(".contain", {duration: 2, delay: 2, text: 'Pesona<br>Warisan'});
@@ -10,6 +11,7 @@ gsap.from(".ani",{ duration: 3, y:100, delay:1.5, opacity: .5});
 gsap.from(".megamendung", {duration: 2, delay:1.5, y:-150, opacity: 0.1});
 gsap.from(".navbar", {duration: 2.5, delay:1.5, y:100, opacity: 1});
 
+// JQuery Code
 $(document).on("scroll",function(){
   if ($(document).scrollTop() > 500){
     $(".introduction").addClass("hidden");
