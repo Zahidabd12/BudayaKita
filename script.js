6c7e3cf4500527d99ca6234d02fb2f8098ab1f91
Ashtacon <astacialvndr@gmail.com>
2023-08-29 00:59:19 +0700

    membuat loading page, memperbaiki css dan javascript, menambahkan sedikit html

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
-gsap.registerPlugin(TextPlugin);
-gsap.to(".introduction", {duration: 3, y:1500, opacity: 1});
-gsap.to(".contain", {duration: 2, delay: .5, text: 'Pesona<br>Warisan'});
-gsap.from(".ani",{ duration: 3, y:55, opacity: 0.5});
-gsap.from(".megamendung", {duration: 2, y:-150, opacity: 0.1});
-gsap.from(".navbar", {duration: 2.5, y:60, opacity: 1});
+window.onload = function() {
+  window.scrollTo(0, 0);
+}
 
+
+gsap.registerPlugin(TextPlugin);
+gsap.to(".introduction", {duration: 3, delay:2, y:1500, opacity: 1});
+gsap.to(".contain", {duration: 2, delay: 2.5, text: 'Pesona<br>Warisan'});
+gsap.from(".ani",{ duration: 3, y:100, delay:2, opacity: .5});
+gsap.from(".megamendung", {duration: 2, delay:2, y:-150, opacity: 0.1});
+gsap.from(".navbar", {duration: 2.5, delay:2, y:100, opacity: 1});
 
 $(document).on("scroll",function(){
-  if ($(document).scrollTop() > 200){
-    $(".introduction").addClass("shrink");
+  if ($(document).scrollTop() > 500){
+    $(".introduction").addClass("hidden");
+    $(".contain").addClass("hidden");
   }else {
-    $(".introduction").removeClass("shrink");
+    $(".introduction").removeClass("hidden");
+    $(".contain").removeClass("hidden");
   }
 });
