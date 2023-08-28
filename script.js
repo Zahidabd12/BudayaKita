8c9103884b9d262782c3c3454ee3b614860ec144
Ashtacon <astacialvndr@gmail.com>
2023-08-29 01:10:03 +0700

    mempercepat loading

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ window.onload = function() {
 
 
 gsap.registerPlugin(TextPlugin);
-gsap.to(".introduction", {duration: 3, delay:2, y:1500, opacity: 1});
-gsap.to(".contain", {duration: 2, delay: 2.5, text: 'Pesona<br>Warisan'});
-gsap.from(".ani",{ duration: 3, y:100, delay:2, opacity: .5});
-gsap.from(".megamendung", {duration: 2, delay:2, y:-150, opacity: 0.1});
-gsap.from(".navbar", {duration: 2.5, delay:2, y:100, opacity: 1});
+gsap.to(".introduction", {duration: 3, delay:1.5, y:1500, opacity: 1});
+gsap.to(".contain", {duration: 2, delay: 2, text: 'Pesona<br>Warisan'});
+gsap.from(".ani",{ duration: 3, y:100, delay:1.5, opacity: .5});
+gsap.from(".megamendung", {duration: 2, delay:1.5, y:-150, opacity: 0.1});
+gsap.from(".navbar", {duration: 2.5, delay:1.5, y:100, opacity: 1});
 
 $(document).on("scroll",function(){
   if ($(document).scrollTop() > 500){
