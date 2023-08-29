0d37624ff329619d7895e9e230b095f5672e40bd
Ashtacon <astacialvndr@gmail.com>
2023-08-29 07:52:06 +0700

    mempersimple animasi

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ window.onload = function() {
 // GSAP Animation
 gsap.registerPlugin(TextPlugin);
 gsap.to(".introduction", {duration: 3, delay:1.5, y:1500, opacity: 1});
-gsap.to(".contain", {duration: 2, delay: 2, text: 'Pesona<br>Warisan'});
+gsap.from(".contain", {duration:2, delay:2.5, opacity:0});
 gsap.from(".ani",{ duration: 3, y:100, delay:1.5, opacity: .5});
 gsap.from(".megamendung", {duration: 2, delay:1.5, y:-150, opacity: 0.1});
 gsap.from(".navbar", {duration: 2.5, delay:1.5, y:100, opacity: 1});
