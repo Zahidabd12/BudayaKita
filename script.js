e4d93c58c7626617b186ca1ee7c91fb34e68a404
Ashtacon <astacialvndr@gmail.com>
2023-09-04 22:20:34 +0700

    ngebug lagi di about -_-

--- script.js
+++ script.js
@@ -6,47 +6,58 @@ window.onload = function() {
 // GSAP Animation
 gsap.registerPlugin(TextPlugin);
 gsap.to(".introduction", {duration: 3, delay:1.5, y:1000, opacity: 0,});
-gsap.from(".contain", {duration:2, delay:2.5, opacity:0});
+//gsap.from(".contain", {duration:2, delay:2.5, opacity:0});
 gsap.from(".ani",{ duration: 3, y:100, delay:1.5, opacity: .5});
 gsap.from(".megamendung", {duration: 2, delay:1.5, y:-150, opacity: 0.1});
 
 // JQuery Code
 $(document).on("scroll",function(){
-  if ($(document).scrollTop() > 150){
-    $(".introduction").addClass("hidden");
-  }else {
-    $(".introduction").removeClass("hidden");
-  }
-  if ($(document).scrollTop() > 750){
-    $(".nav").addClass("fixed-top");   
-  }else {
-    $(".nav").removeClass("fixed-top");
-  }
+
+
+    if ($(document).scrollTop() > 100){
+      $(".contain").addClass("geser");
+    }else {
+      $(".contain").removeClass("geser");
+    }
+    if ($(document).scrollTop() > 150){
+      $(".introduction").addClass("hidden");
+    }else {
+      $(".introduction").removeClass("hidden");
+    }
+    if ($(document).scrollTop() > 750){
+      $(".nav").addClass("fixed-top");   
+    }else {
+      $(".nav").removeClass("fixed-top");
+    }
+
+  $(".slider").slick({
+
+    // normal options...
+    infinite: false,
+  
+    // the magic
+    responsive: [{
+  
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          infinite: true
+        }
+  
+      }, {
+  
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          dots: true
+        }
+  
+      }, {
+  
+        breakpoint: 300,
+        settings: "unslick" // destroys slick
+  
+      }]
+  });
 });
 
-//swiper JS
-var swiper = new Swiper(".mySwiper", {
-  spaceBetween: 30,
-  effect: "fade",
-  grabCursor: true,
-  centeredSlides: true,
-  slidesPerView: "auto",
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-    /*autoplay: {
-        delay: 5000,
-        disableOnInteraction: false,
-      },
-    */
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
-  });
