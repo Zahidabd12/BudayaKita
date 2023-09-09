0e48020497ef364c6da0dc903b32151c2433e8dc
Ashtacon <astacialvndr@gmail.com>
2023-09-09 09:50:58 +0700

    mengubah sedikit animasi

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
 // Native Code
-window.onload = function() {
-  window.scrollTo(0, 0);
-}
-
 // GSAP Animation
 gsap.registerPlugin(TextPlugin);
-gsap.to(".introduction", {duration: 3, delay:1.5, y:1000, opacity: 0,});
-//gsap.from(".contain", {duration:2, delay:2.5, opacity:0});
-gsap.from(".ani",{ duration: 3, y:100, delay:1.5, opacity: .5});
-gsap.from(".megamendung", {duration: 2, delay:1.5, y:-150, opacity: 0.1});
+gsap.to(".introduction", {duration: 2, delay:1.5, opacity: 0,});
+gsap.to(".contain", {duration:2, delay:1.5, opacity:1});
+gsap.from(".ani",{ duration: 3, y:100, delay:1, opacity: .5});
+gsap.from("#navbar", {duration: 2, delay:2, opacity: 0});
+gsap.from(".megamendung", {duration: 2, delay:1, y:-150, opacity: 0.1});
 
 // JQuery Code
 $(document).on("scroll",function(){
@@ -25,39 +22,9 @@ $(document).on("scroll",function(){
       $(".introduction").removeClass("hidden");
     }
     if ($(document).scrollTop() > 750){
-      $(".nav").addClass("fixed-top");   
+      $(".nav").addClass("fixed-top");     
     }else {
       $(".nav").removeClass("fixed-top");
     }
-
-  $(".slider").slick({
-
-    // normal options...
-    infinite: false,
-  
-    // the magic
-    responsive: [{
-  
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          infinite: true
-        }
-  
-      }, {
-  
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          dots: true
-        }
-  
-      }, {
-  
-        breakpoint: 300,
-        settings: "unslick" // destroys slick
-  
-      }]
-  });
 });
 
