1672883fea98361eafd6821e0b9fa251e23c397e
Ashtacon <astacialvndr@gmail.com>
2023-09-15 01:35:38 +0700

    fixed bug in jumbotron + added loading + fixed non-responsive bug at about

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Native Code
 // GSAP Animation
 gsap.registerPlugin(TextPlugin);
-gsap.to(".introduction", {duration: 2, delay:1.5, opacity: 0,});
+gsap.to(".introduction", {duration: 2, delay:.5, opacity: 0,});
 gsap.to(".contain", {duration:2, delay:1.5, opacity:1});
 gsap.from(".ani",{ duration: 3, y:100, delay:1, opacity: .5});
 gsap.from("#navbar", {duration: 2, delay:2, opacity: 0});
@@ -9,22 +9,19 @@ gsap.from(".megamendung", {duration: 2, delay:1, y:-150, opacity: 0.1});
 
 // JQuery Code
 $(document).on("scroll",function(){
+    setTimeout(function() {
+      $(".introduction").addClass("hidden");
+    }, 2000);
 
-
+    
     if ($(document).scrollTop() > 100){
       $(".contain").addClass("geser");
     }else {
       $(".contain").removeClass("geser");
     }
-    if ($(document).scrollTop() > 150){
-      $(".introduction").addClass("hidden");
-    }else {
-      $(".introduction").removeClass("hidden");
-    }
-    if ($(document).scrollTop() > 750){
+    if ($(document).scrollTop() > 800){
       $(".nav").addClass("fixed-top");     
     }else {
       $(".nav").removeClass("fixed-top");
     }
 });
-
